36307c7d7aa984c60499f816c5d288f7de1e0490
Kevin Warui <waruimacharia@gmail.com>
2024-03-20 18:55:42 +0300

    Edit 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
 
-SELECT origin, fans AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP by origin
 ORDER by nb_fans DESC;
