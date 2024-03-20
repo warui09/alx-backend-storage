b8b96027077b745586a8b5222ffac1a2e10d3681
Kevin Warui <waruimacharia@gmail.com>
2024-03-20 16:48:54 +0000

    Add 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
-
-SELECT band_name, DATE_FORMAT(FROM_DAYS(DATEDIFF(COALESCE(split, "2022"), formed)), "%Y") as lifespan
-WHERE style='Glam rock'
+SELECT band_name, IFNULL(split, "2022") - formed AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
