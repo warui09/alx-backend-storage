dd15a02ce158ef0c3db77655435ea47a95dc8434
Kevin Warui <waruimacharia@gmail.com>
2024-03-21 15:20:31 +0000

    Refactor 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,17 +1,27 @@
 -- creates a stored procedure that computes and stores
 -- the average score for a student
 DELIMITER //
+
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE total_score FLOAT;
+	DECLARE total_score INT;
 	DECLARE total_projects INT;
 
-	SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = user_id;
-	SELECT COUNT(*) INTO total_projects FROM corrections WHERE user_id = usr_id;
+	SELECT SUM(score)
+		INTO total_score
+		FROM corrections
+		WHERE corrections.user_id = user_id;
 
-	UPDATE users SET average_score = IF(projects_count = 0, 0, total_score / total_projects)
-	WHERE users.id = user_id;
+	SELECT COUNT(*)
+		INTO total_projects
+		FROM corrections
+		WHERE corrections.user_id = usr_id;
+
+	UPDATE users
+		SET average_score = IF(projects_count = 0, 0, total_score / total_projects)
+		WHERE users.id = user_id;
 	
 END //
-DELIMITER;
+DELIMITER ;
