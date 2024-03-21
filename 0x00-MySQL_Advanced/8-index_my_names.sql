b8a84537d68c9302063d42118448d9ec2450320f
Kevin Warui <waruimacharia@gmail.com>
2024-03-21 16:50:25 +0000

    Edit 8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,9 +1,2 @@
 -- creates an index on the table names
-ALTER TABLE names
-ADD first_letter CHAR(1);
-
-UPDATE names
-SET first_letter = LEFT(name, 1);
-
-CREATE INDEX idx_name_first
-ON names (first_letter);
+CREATE INDEX idx_name_first ON names(name(1));
