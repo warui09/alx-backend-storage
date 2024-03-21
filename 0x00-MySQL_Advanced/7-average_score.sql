3cf5075cb939ab13d2eaffe572c32f59fc73f6e6
Kevin Warui <waruimacharia@gmail.com>
2024-03-21 15:16:00 +0000

    Refactor 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,8 +4,14 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE avg_score FLOAT;
-	SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
-	UPDATE userS SET average_score = avg_score WHERE id = user_id;
+	DECLARE total_score FLOAT;
+	DECLARE total_projects INT;
+
+	SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = user_id;
+	SELECT COUNT(*) INTO total_projects FROM corrections WHERE user_id = usr_id;
+
+	UPDATE users SET average_score = IF(projects_count = 0, 0, total_score / total_projects)
+	WHERE users.id = user_id;
+	
 END //
 DELIMITER;
