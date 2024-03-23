97554ccd0da318bd033a922d57b529034744845d
Kevin Warui <waruimacharia@gmail.com>
2024-03-23 21:57:23 +0000

    Add 100-average_weighted_score.sql

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,30 @@
+-- creates a stored procedure that computes and store the
+-- average weighted score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+	DECLARE sum_weights INT DEFAULT 0;
+	DECLARE sum_weighted_score FLOAT DEFAULT 0;
+
+	SELECT SUM(corrections.score * projects.weight)
+		INTO weighted_score
+		FROM corrections
+			INNER JOIN projects
+			ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+	SELECT SUM(projects.weight)
+		INTO sum_weights
+		FROM corrections
+			INNER JOIN projects
+			ON corrections.project_id = projects.id
+		WHERE corrections.user_id = user_id;
+
+	UPDATE users
+		SET users.average_score = sum_weighted_score / sum_weights
+		WHERE users.id = user_id;
+
+
+END //
+DELIMITER ;
