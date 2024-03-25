b25c6f31e423b030711876149a3a18080eeb458f
Kevin Warui <waruimacharia@gmail.com>
2024-03-25 10:36:34 -0700

    Add 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,12 +3,8 @@
 
 import pymongo
 
+
 def insert_school(mongo_collection, **kwargs):
-    """ inserts a new document in a collection based on kwargs """
+    """inserts a new document in a collection based on kwargs"""
 
-    new_school = {}
-
-    for key, value in kwargs.items():
-        new_school.update({key: value})
-
-    mongo_collection.insert_one(new_school)    
+    return mongo_collection.insert_one(kwargs).inserted_id
