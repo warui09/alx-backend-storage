92d793c95cb266b8c67426a2ae14ab53d6041ad0
Kevin Warui <waruimacharia@gmail.com>
2024-03-26 10:10:00 -0700

    Add 102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" task 15 solution """
+
+from pymongo import MongoClient
+
+
+def show_stats():
+    """provides some stats about Nginx logs stored in MongoDB"""
+
+    client = MongoClient("localhost")
+    db = client.logs
+
+    call = db.command("dbstats")
+    objects = call["objects"]
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    pipeline = [
+        {"$group": {"_id": "$ip", "total": {"$sum": 1}}},
+        {"$sort": {"total": -1}},
+        {"$limit": 10},
+    ]
+
+    ips = db.nginx.aggregate(pipeline)
+
+    print(f"{objects} logs")
+    print("Methods:")
+    for method in methods:
+        count = len(list(db.nginx.find({"method": method})))
+        print("{:<3} method {}: {}".format("", method, count))
+
+    status_checks = len(list(db.nginx.find({"method": "GET", "path": "/status"})))
+    print(f"{status_checks} status checks")
+
+    print("IPs:")
+    for ip in ips:
+        print("{:<3}{}: {}".format("", ip.get("_id"), ip.get("total")))
+
+
+if __name__ == "__main__":
+    """run script directly"""
+
+    show_stats()
