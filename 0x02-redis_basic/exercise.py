06b8364b0506ee75610ed1de77f300a4d35a5358
Kevin Warui <waruimacharia@gmail.com>
2024-03-27 11:21:22 +0000

    Add exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" task 0 solution """
+
+from __future__ import annotations
+import redis
+from typing import Union
+from uuid import uuid4
+
+
+class Cache:
+    """cache class"""
+
+    def __init__(self: Cache) -> None:
+        """class initiliazer"""
+
+        self._redis = redis.Redis()
+
+    def store(self: Cache, data: Union[str, bytes, int, float]) -> str:
+        """put data in Redis using a random key"""
+
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
