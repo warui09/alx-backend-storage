f697a733df0ee922a52f3a0870461f48f4adb2d3
Kevin Warui <waruimacharia@gmail.com>
2024-03-27 11:29:00 +0000

    Remove type annotation for class Cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ task 0 solution """
 
-from __future__ import annotations
 import redis
 from typing import Union
 from uuid import uuid4
@@ -10,13 +9,13 @@ from uuid import uuid4
 class Cache:
     """cache class"""
 
-    def __init__(self: Cache) -> None:
+    def __init__(self) -> None:
         """class initiliazer"""
 
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self: Cache, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """put data in Redis using a random key"""
 
         key = str(uuid4())
